ad0e8ed4e917d7511f0b57ea6f77a365a4b5b2c1
jfsenechal <jfsenechal@gmail.com>
2022-09-22 20:58:31 +0200

    clean

--- src/components/Header/Inside/Header.vue
+++ src/components/Header/Inside/Header.vue
@@ -4,7 +4,7 @@ import IconesSocial from "@/components/Header/Inside/IconesSocial.vue";
 </script>
 <template>
   <header class="w-full h-14 p-6 shadow-2xl flex justify-end items-center">
-    <img src="public/images/img_logo.png" alt="logo" class="w-24 h-14">
+    <img src="images/img_logo.png" alt="logo" class="w-24 h-14">
     <Navigation/>
     <IconesSocial/>
    </header>
