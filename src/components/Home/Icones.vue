8c03bd11800dc59cc57fb6bde09bfce5ddc3643e
jfsenechal <jfsenechal@gmail.com>
2022-09-30 19:49:06 +0200

    icones css bug hover:

--- src/components/Home/Icones.vue
+++ src/components/Home/Icones.vue
@@ -1,8 +1,13 @@
 <script setup>
-import IconeItem from "@/components/Home/IconeItem.vue";
-</script>
+import IconeItem from "@/components/Home/IconeItem.vue";</script>
 <template>
-  <ul class="flex w-full pt-14">
+  <ul class="mt-8 w-[32rem] grid grid-cols-[repeat(4,minmax(7rem,7rem))] gap-6">
     <IconeItem title="Piscine" :icon="'i-swimmer'" :iconHover="'i-swimmer-white'"/>
+    <IconeItem title="Environnement Déchet" :icon="'i-leaf'" :iconHover="'i-leaf-white'"/>
+    <IconeItem title="Travaux Arrêtés de Police" :icon="'i-traffic-cone'" :iconHover="'i-traffic-cone-white'"/>
+    <IconeItem title="Enfance Jeunesse" :icon="'i-beach-ball'" :iconHover="'i-beach-ball-white'"/>
+    <IconeItem title="Carte dynamique" :icon="'i-map'" :iconHover="'i-map-white'"/>
+    <IconeItem title="CPAS" :icon="'i-handshake'" :iconHover="'i-handshake-white'"/>
+    <IconeItem title="Mon adresse e-mail" :icon="'i-envelope'" :iconHover="'i-envelope-white'"/>
   </ul>
 </template>
