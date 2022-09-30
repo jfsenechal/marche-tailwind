7c619d5ce630819f80bc3a813dc01a89d4d229ea
jfsenechal <jfsenechal@gmail.com>
2022-09-30 09:06:32 +0200

    icones home draft

---
+++ src/components/Home/Icones.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="w-full flex ">
+    <li class="flex flex-grow-0 flex-shrink-0 justify-center basis-1/4 text-center group">
+      <a href="/sport/piscine-communale" class="icon_custom">
+        <i
+            class="block w-20 h-20 shadow-xl mx-auto rounded-full i-swimmer group-hover:i-swimmer-white bg-white bg-no-repeat bg-center
+          group-hover:bg-blue-light hover:text-white i-bg-color-transition bg-[length:36px_36px]"></i>
+        <span
+            class=" mx-auto inline-block text-black font-montserrat-bold leading-4 pt-3">Piscine</span>
+      </a>
+    </li>
+    <li class="flex flex-grow-0 flex-shrink-0 justify-center basis-1/4 text-center group">
+      <a href="/sport/piscine-communale" class="icon_custom">
+        <i
+            class="block w-20 h-20 shadow-xl mx-auto rounded-full i-swimmer bg-white bg-no-repeat bg-center
+          group-hover:bg-blue-light hover:text-white i-bg-color-transition p-4"></i>
+        <span
+            class=" mx-auto inline-block text-black font-montserrat-bold leading-4 pt-3">Piscine</span>
+      </a>
+    </li>
+  </ul>
+  <ul class="w-full flex max-w-lg justify-center pt-10">
+    <li class="flex justify-center items-center w-[20%] text-center">
+      <a href="#">
+        <img class="bg-white h-14 w-14 rounded-full mx-auto hover:bg-blue-light"
+             src="https://www.marche.be/wp-content/themes/marchebe/assets/tartine/rsc/svg/icon_swimmer_primary.svg"
+             alt="patrimoine"/>
+        <span class="text-black mx-auto">Patrimoine</span>
+      </a>
+    </li>
+    <li
+        class="flex relative flex-grow-0 flex-shrink-0 justify-center py-0 px-4 m-0 w-full
+         font-sans text-base not-italic font-light text-center align-baseline border-0 basis-1/4 text-neutral-800"
+        style="text-rendering: optimizelegibility; line-height: 150%; max-width: 25%; list-style: outside none none;"
+    ></li>
+  </ul>
+</template>
