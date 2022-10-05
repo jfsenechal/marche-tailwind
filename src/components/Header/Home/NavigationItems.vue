e542ce86efe05f4b5bfd118897e24f9b7c87d811
jfsenechal <jfsenechal@gmail.com>
2022-10-05 10:44:17 +0200

    add color

--- src/components/Header/Home/NavigationItems.vue
+++ src/components/Header/Home/NavigationItems.vue
@@ -1,22 +1,23 @@
 <script setup>
 const items = [
-  {'title': 'Administration', 'id': 1},
-  {'title': 'Citoyen', 'id': 2},
-  {'title': 'Culture', 'id': 3},
-  {'title': 'Économie', 'id': 4},
-  {'title': 'Enfance-Jeunesse', 'id': 5},
-  {'title': 'Santé', 'id': 6},
-  {'title': 'Social', 'id': 7},
-  {'title': 'Sport', 'id': 8},
-  {'title': 'Tourisme', 'id': 9},
+  {'title': 'Administration', 'color': 'administration', 'id': 1},
+  {'title': 'Citoyen', 'color': 'citoyen', 'id': 2},
+  {'title': 'Culture', 'color': 'culture', 'id': 3},
+  {'title': 'Économie', 'color': 'economie', 'id': 4},
+  {'title': 'Enfance-Jeunesse', 'color': 'jeunesse', 'id': 5},
+  {'title': 'Santé', 'color': 'sante', 'id': 6},
+  {'title': 'Social', 'color': 'social', 'id': 7},
+  {'title': 'Sport', 'color': 'sport', 'id': 8},
+  {'title': 'Tourisme', 'color': 'tourisme', 'id': 9},
 ]
 </script>
 <template>
-  <div class="relative w-full h-52 flex flex-col md:flex-row">
+  <div class="w-full h-52 flex flex-col md:flex-row">
     <div class="split basis-1/2 bg-slate-500">
       <ul class="flex flex-col items-end pr-8 ">
         <li v-for="item in items" :key="item.id"
-            class="flex-grow text-white hover:bg-white hover:text-cta-dark flex flex-col items-end w-full">
+            class="relative p-2 font-montserrat-semi-bold text-white hover:bg-white hover:text-citoyen flex flex-col items-end w-full">
+           <!-- :class="'hover:text-'+ item.color">-->
           <a href="/">{{ item.title }}</a>
         </li>
       </ul>
