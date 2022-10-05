bf01117128eb18ce9a4718d4f445e1c90604c9d8
jfsenechal <jfsenechal@gmail.com>
2022-10-05 22:18:11 +0200

    separate data

--- src/components/Header/Home/NavigationItems.vue
+++ src/components/Header/Home/NavigationItems.vue
@@ -1,84 +1,12 @@
 <script setup>
 import { ref } from "vue";
-
-const items = [
-  {
-    "title": "Administration", "color": "hover:text-administration", "id": 1, "childs":
-      [
-        { "title": "Conseil" },
-        { "title": "Collège" }
-      ]
-  },
-  {
-    "title": "Citoyen", "color": "hover:text-citoyen", "id": 2, "childs":
-      [
-        { "title": "Environnement" },
-        { "title": "Justice" }
-      ]
-  },
-  {
-    "title": "Culture", "color": "hover:text-culture", "id": 3, "childs":
-      [
-        { "title": "Musées" },
-        { "title": "Cinéma" }
-      ]
-  },
-  {
-    "title": "Économie", "color": "hover:text-economie", "id": 4, "childs":
-      [
-        { "title": "Commerces" },
-        { "title": "Parking" }
-      ]
-  },
-  {
-    "title": "Enfance-Jeunesse", "color": "hover:text-jeunesse", "id": 5, "childs":
-      [
-        { "title": "Crèches" },
-        { "title": "Activités" }
-      ]
-  },
-  {
-    "title": "Santé", "color": "hover:text-sante", "id": 6, "childs":
-      [
-        { "title": "Médecins" },
-        { "title": "Vétos" }
-      ]
-  },
-  {
-    "title": "Social", "color": "hover:text-social", "id": 7, "childs":
-      [
-        { "title": "Cpas" },
-        { "title": "Associations" }
-      ]
-  },
-  {
-    "title": "Sport", "color": "hover:text-sport", "id": 8, "childs":
-      [
-        { "title": "Clubs" },
-        { "title": "Piste" }
-      ]
-  },
-  {
-    "title": "Tourisme", "color": "hover:text-tourisme", "id": 9, "childs":
-      [
-        { "title": "Restos" },
-        { "title": "Promenade" }
-      ]
-  }
-];
+import { items } from "@/items";
 
 const state = ref(0);
 
-function isActive(id) {
-  return state === id;
-}
-
 function setActive(id) {
-  console.log(id)
   state.value = id
-  console.log(state.value)
 }
-
 </script>
 
 <template>
