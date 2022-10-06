5f42cdefaf60121788b514c78656753d4388f926
jfsenechal <jfsenechal@gmail.com>
2022-10-06 11:11:11 +0200

    fix id

--- src/components/Home/Partners.vue
+++ src/components/Home/Partners.vue
@@ -1,12 +1,12 @@
 <script setup>
 const partners = [
   {'url': 'https://cap.marche.be', 'img': '../images/img/img_sponsor02.png', 'alt': 'cap commerçant', 'id': 1},
-  {'url': 'https://www.visitmarche.be', 'img': '../images/img/visit_logo_b.png', 'alt': 'visit marche', 'id': 1},
-  {'url': '/economie', 'img': '../images/img/img_sponsor04.png', 'alt': 'economie', 'id': 1},
-  {'url': 'http://www.paysdefamenne.be', 'img': '../images/img/img_sponsor03.png', 'alt': 'pays de famenne', 'id': 1},
-  {'url': 'https://www.famenneardenne.be', 'img': '../images/img/img_sponsor01.png', 'alt': 'famenne ardenne', 'id': 1},
-  {'url': 'https://mcfa.marche.be', 'img': '../images/img/img_sponsor05.png', 'alt': 'mcfa', 'id': 1},
-  {'url': 'https://artistes.marche.be', 'img': '../images/img/artistes.png', 'alt': 'artistes', 'id': 1},
+  {'url': 'https://www.visitmarche.be', 'img': '../images/img/visit_logo_b.png', 'alt': 'visit marche', 'id': 2},
+  {'url': '/economie', 'img': '../images/img/img_sponsor04.png', 'alt': 'economie', 'id': 3},
+  {'url': 'http://www.paysdefamenne.be', 'img': '../images/img/img_sponsor03.png', 'alt': 'pays de famenne', 'id': 4},
+  {'url': 'https://www.famenneardenne.be', 'img': '../images/img/img_sponsor01.png', 'alt': 'famenne ardenne', 'id': 5},
+  {'url': 'https://mcfa.marche.be', 'img': '../images/img/img_sponsor05.png', 'alt': 'mcfa', 'id': 6},
+  {'url': 'https://artistes.marche.be', 'img': '../images/img/artistes.png', 'alt': 'artistes', 'id': 7},
 ]
 </script>
 <template>
