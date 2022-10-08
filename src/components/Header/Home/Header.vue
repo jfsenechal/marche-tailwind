9991a1b0d40412e746b90eb957c0ee8eaa35e6bf
jfsenechal <jfsenechal@gmail.com>
2022-10-08 16:26:28 +0200

    add emit open-search

--- src/components/Header/Home/Header.vue
+++ src/components/Header/Home/Header.vue
@@ -1,10 +1,18 @@
 <script setup>
+import { defineEmits } from "vue";
 import Navigation from "@/components/Header/Home/Navigation.vue";
-import IconesSocial from "@/components/Header/Home/IconesSocial.vue";</script>
+import IconesSocial from "@/components/Header/Home/IconesSocial.vue";
+
+const emit = defineEmits(['open-search'])
+
+function updateOpenSearch() {
+  emit("open-search")
+}
+</script>
 <template>
   <header class="flex h-20 w-full items-center justify-end p-6 shadow-xl z-10 relative" id="headerTop">
     <img src="images/img_logo.png" alt="logo" class="h-14 w-24" />
-    <Navigation />
+    <Navigation @open-search="updateOpenSearch" />
     <IconesSocial />
   </header>
 </template>
