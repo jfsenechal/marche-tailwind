b7be1f9a3e184ae8cb77367a3a3c262b87d44f4d
jfsenechal <jfsenechal@gmail.com>
2022-10-11 15:37:29 +0200

    add search header on mobile

--- src/components/Header/Home/Header.vue
+++ src/components/Header/Home/Header.vue
@@ -2,6 +2,7 @@
 import { defineEmits } from "vue";
 import Navigation from "@/components/Header/Home/Navigation.vue";
 import IconesSocial from "@/components/Header/Home/IconesSocial.vue";
+import HeaderSearch from "@/components/Mobile/HeaderSearch.vue";
 
 const emit = defineEmits(['open-search'])
 
@@ -13,6 +14,7 @@ function updateOpenSearch() {
   <header class="flex h-20 w-full items-center justify-end p-6 shadow-xl z-10 relative" id="headerTop">
     <img src="images/img_logo.png" alt="logo" class="h-14 w-24 mr-auto" />
     <Navigation @open-search="updateOpenSearch" />
+    <HeaderSearch/>
     <IconesSocial />
   </header>
 </template>
