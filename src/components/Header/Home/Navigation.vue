5c0712e43a7474b4af1404fc382981e740e62d27
jfsenechal <jfsenechal@gmail.com>
2022-10-11 15:29:36 +0200

    add nav

--- src/components/Header/Home/Navigation.vue
+++ src/components/Header/Home/Navigation.vue
@@ -5,35 +5,37 @@ import IconMenuVivreSvg from "@/components/Header/Home/IconMenuVivreSvg.vue";
 defineEmits(["open-search"]);
 </script>
 <template>
-  <ul class="hidden xl:flex" id="navigationTop">
-    <li class="nav-top font-montserrat-semi-bold displaymenu transition-all duration-500">
-      <a href="#">
-        <IconMenuVivreSvg />
-        Vivre à Marche
-      </a>
-      <NavigationVivre />
-    </li>
-    <li class="nav-top text-cta-dark font-montserrat-semi-bold">
-      <a href="#">E-guichet</a>
-    </li>
-    <li class="nav-top font-montserrat-semi-bold">
-      <a href="#">Actualités</a>
-    </li>
-    <li class="nav-top font-montserrat-semi-bold">
-      <a href="#">Horaires-Contact</a>
-    </li>
-    <li class="nav-top font-montserrat-semi-bold">
-      <a href="#">Agenda</a>
-    </li>
-    <li class="nav-top font-montserrat-semi-bold">
-      <a href="#">Inondation</a>
-    </li>
-    <li class="nav-top font-montserrat-semi-bold">
-      <a href="/" @click.prevent="$emit('open-search')">
-        <i class="fa fa-search"></i>
-      </a>
-    </li>
-  </ul>
+  <nav>
+    <ul class="hidden xl:flex" id="navigationTop">
+      <li class="nav-top font-montserrat-semi-bold displaymenu transition-all duration-500">
+        <a href="#">
+          <IconMenuVivreSvg />
+          Vivre à Marche
+        </a>
+        <NavigationVivre />
+      </li>
+      <li class="nav-top text-cta-dark font-montserrat-semi-bold">
+        <a href="#">E-guichet</a>
+      </li>
+      <li class="nav-top font-montserrat-semi-bold">
+        <a href="#">Actualités</a>
+      </li>
+      <li class="nav-top font-montserrat-semi-bold">
+        <a href="#">Horaires-Contact</a>
+      </li>
+      <li class="nav-top font-montserrat-semi-bold">
+        <a href="#">Agenda</a>
+      </li>
+      <li class="nav-top font-montserrat-semi-bold">
+        <a href="#">Inondation</a>
+      </li>
+      <li class="nav-top font-montserrat-semi-bold">
+        <a href="/" @click.prevent="$emit('open-search')">
+          <i class="fa fa-search"></i>
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
 <style>
 li.nav-top {
