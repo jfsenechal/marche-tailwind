85f34b01cdc1b2b1637e62e6bead06112e72937b
jfsenechal <jfsenechal@gmail.com>
2022-10-11 16:30:41 +0200

    add for mobile

--- src/components/Mobile/HeaderSearch.vue
+++ src/components/Mobile/HeaderSearch.vue
@@ -2,8 +2,7 @@
 defineEmits(["open-search"]);
 </script>
 <template>
-  <div class="flex xl:hidden ">
-    <form name="searching" class="mx-auto flex w-[35rem]">
+    <form name="searching" class="flex items-center justify-center xl:hidden mx-auto">
       <label class="block" for="search">
         <span class="sr-only">Rechercher</span>
       </label>
@@ -12,13 +11,8 @@ defineEmits(["open-search"]);
         id="search"
         name="s"
         autocomplete="off"
-        placeholder="Que cherchez-vous ?"
-        class="block flex-grow rounded-full border border-gray-300 bg-white py-2 pl-9 pr-16 font-light leading-5 text-teal-300 placeholder:italic placeholder:text-gray-400 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500"
+        placeholder="Rechercher"
+        class="w-full mx-4 rounded-full border border-gray-300 font-light leading-5 text-teal-300 placeholder:italic placeholder:text-gray-400 "
       />
-      <button
-        type="submit"
-        class="i-search relative right-12 top-2 h-6 w-6 bg-cover bg-no-repeat p-2 pl-4"
-      ></button>
     </form>
-  </div>
 </template>
