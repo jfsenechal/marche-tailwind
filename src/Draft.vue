b83d7b1e7cedcac75606d6a19a571f01af0c5f27
jfsenechal <jfsenechal@gmail.com>
2022-10-16 22:05:29 +0200

    essai menu top pc hover

--- src/Draft.vue
+++ src/Draft.vue
@@ -1,12 +1,6 @@
 <script setup>
 import { ref } from "vue";
 import { items } from "@/items";
-
-const state = ref(4);
-
-function setActive(id) {
-  state.value = id;
-}
 </script>
 
 <template>
@@ -16,17 +10,17 @@ function setActive(id) {
         v-for="item in items"
         :data-top-id="item.id"
         :key="item.id"
-        class="pi flex flex-col basis-full border border-red-600 gr45oup hover:bg-white font-montserrat-semi-bold leading-8 text-white"
+        class="py-1 pi flex flex-col basis-full border border-red-600 gr45oup hover:bg-white font-montserrat-semi-bold leading-8 text-white"
         :class="item.color">
         <a href="/" class="mr-6 whitespace-nowrap ml-auto">{{ item.title }}</a>
         <div
           :data-menu-id="item.id"
-          class="absolute top-0 bottom-0 left-1/2 right-0 border border-amber-500 group-hover:bg-white">
+          class="hidden absolute top-0 bottom-0 left-1/2 right-0 border border-amber-500 group-hover:bg-white">
           <ul class="w-full p-8">
             <li
               v-for="child in item.childs"
               :key="child.id"
-              :class="item.color">
+              class="text-cta-dark">
               {{ child.title }}
             </li>
           </ul>
@@ -39,18 +33,17 @@ function setActive(id) {
 li.pi {
   position: initial;
 }
-li[data-top-id='4']:hover > div {
-  background-color: red;
-}
-.max-w-inerit {
-  max-width: inherit;
-}
-
-#checkboxMain:checked ~ div.sousitems {
-  @apply left-0;
-}
-
-#checkboxMain {
-
+li[data-top-id='1']:hover > div,
+li[data-top-id='2']:hover > div,
+li[data-top-id='3']:hover > div,
+li[data-top-id='4']:hover > div,
+li[data-top-id='5']:hover > div,
+li[data-top-id='6']:hover > div,
+li[data-top-id='7']:hover > div,
+li[data-top-id='8']:hover > div,
+li[data-top-id='9']:hover > div {
+  opacity: 100;
+  background-color: white;
+  display: block;
 }
 </style>
