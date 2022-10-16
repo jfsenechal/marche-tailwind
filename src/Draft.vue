80f49005ff4f1bec3ce75fba229d2c4540e57017
jfsenechal <jfsenechal@gmail.com>
2022-10-16 21:42:51 +0200

    essai menu top pc hover

--- src/Draft.vue
+++ src/Draft.vue
@@ -14,11 +14,14 @@ function setActive(id) {
     <ul class="flex flex-col items-stretch justify-start h-auto box-border max-w-[50%]">
       <li
         v-for="item in items"
+        :data-top-id="item.id"
         :key="item.id"
-        class="pi flex flex-col basis-full border border-red-600 group hover:bg-white"
+        class="pi flex flex-col basis-full border border-red-600 gr45oup hover:bg-white font-montserrat-semi-bold leading-8 text-white"
         :class="item.color">
-        <a href="/" class="mr-6 whitespace-nowrap ml-auto mr-6">{{ item.title }}</a>
-        <div class="absolute top-0 bottom-0 left-1/2 right-0 border border-amber-500 group-hover:bg-white">
+        <a href="/" class="mr-6 whitespace-nowrap ml-auto">{{ item.title }}</a>
+        <div
+          :data-menu-id="item.id"
+          class="absolute top-0 bottom-0 left-1/2 right-0 border border-amber-500 group-hover:bg-white">
           <ul class="w-full p-8">
             <li
               v-for="child in item.childs"
@@ -36,7 +39,9 @@ function setActive(id) {
 li.pi {
   position: initial;
 }
-
+li[data-top-id='4']:hover > div {
+  background-color: red;
+}
 .max-w-inerit {
   max-width: inherit;
 }
