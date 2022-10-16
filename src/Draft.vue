9732ef8462cdd440ee965f5c49dc9d39241323b1
jfsenechal <jfsenechal@gmail.com>
2022-10-16 20:59:37 +0200

    essai menu top pc

--- src/Draft.vue
+++ src/Draft.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { ref } from "vue";
-import { items } from "@/items";
 
 const state = ref(4);
 
@@ -10,12 +9,12 @@ function setActive(id) {
 </script>
 
 <template>
-  <div class="bg-cta-dark fixed inset-0 m-auto overflow-hidden w-full">
-    <ul class="w-full items-start justify-end ml-auto h-auto box-border">
-      <li class="pi max-w-[50%] flex flex-col flex-shrink-0 flex-grow-0 basis-2/5 border border-red-600 hover:bg-white">
-        <a href="/">Administration</a>
-        <div class="absolute top-0 bottom-0 left-1/2 m-0 right-0 border border-amber-500">
-          <ul class="w-full">
+  <div class="bg-cta-dark fixed inset-0 m-au45to overflow-hidden w-full">
+    <ul class="flex items-stretch justify-start h-auto box-border max-w-[50%]">
+      <li class="pi flex flex-col basis-full border border-red-600 group hover:bg-white">
+        <a href="/" class="ml-auto mr-6">Administration</a>
+        <div class="absolute top-0 bottom-0 left-1/2 right-0 border border-amber-500 group-hover:bg-white">
+          <ul class="w-full p-8">
             <li>un</li>
             <li>deux</li>
             <li>trois</li>
@@ -29,9 +28,11 @@ function setActive(id) {
 li.pi {
   position: initial;
 }
+
 .max-w-inerit {
   max-width: inherit;
 }
+
 #checkboxMain:checked ~ div.sousitems {
   @apply left-0;
 }
