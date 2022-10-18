9d941fa3c8790e4a9ed2bfd33714006acd7b0b05
jfsenechal <jfsenechal@gmail.com>
2022-10-18 21:37:31 +0200

    nice title

---
+++ src/components/Mobile/NavigationBackMenuMobile.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3 class="text-xl text-administration font-montserrat-semi-bold">
+    <slot></slot>
+  </h3>
+  <a href="/" class="flex w-full flex-row items-center pl-6 py-4">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+         stroke="currentColor"
+         class="w-6 h-6 text-cta-light">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+    </svg>
+    <span class="text-cta-dark">Retour sur Vivre à Marche</span>
+  </a>
+</template>
