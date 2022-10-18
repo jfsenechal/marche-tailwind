9f1af9caab193144f9239222d02a50a3fe50d713
jfsenechal <jfsenechal@gmail.com>
2022-10-18 11:42:04 +0200

    integration

--- src/components/Header/Home/NavigationVivre.vue
+++ src/components/Header/Home/NavigationVivre.vue
@@ -1,56 +1,87 @@
 <script setup>
-import { ref } from "vue";
-import { items } from "@/items";
+import { onMounted, ref } from "vue";
 
-const state = ref(0);
+const mainItems = ref([]);
 
-function setActive(id) {
-  state.value = id;
+function getItems() {
+  fetch("https://www.marche.be/api/menu.php")
+    .then(function(response) {
+      // The API call was successful!
+      return response.json();
+    })
+    .then(function(data) {
+      // This is the JSON from our response
+      mainItems.value = data;
+    })
+    .catch(function(err) {
+      // There was an error
+      console.warn("Something went wrong.", err);
+    });
 }
+
+onMounted(() => {
+  getItems();
+});
 </script>
 
 <template>
-  <div
-    class="showme fixed top-[4.3rem] left-0 right-0 bottom-auto z-10 grid hidden grid-cols-2 overflow-hidden bg-cta-dark transition-all duration-500"
-    id="menuVivre"
-  >
-    <div class="py-4">
-      <ul class="flex flex-col items-start">
-        <li
-          v-for="item in items"
-          :key="item.id"
-          class="flex w-full flex-col items-end font-montserrat-semi-bold leading-8 text-white hover:bg-white"
-          :class="item.color"
-        >
-          <a
-            href="#"
-            class="mr-6 whitespace-nowrap text-left"
-            @click="setActive(item.id)"
-          >
-            {{ item.title }}
-          </a>
-        </li>
-      </ul>
-    </div>
-    <div class="grid bg-white">
-      <ul
-        v-for="item in items"
-        :key="item.id"
-        :data-menu-id="item.id"
-        :id="'menu' + item.id"
-        v-show="state === item.id"
-        class="col-start-1 col-end-2 row-start-1 row-end-2 px-4 py-4"
-      >
-        <li
-          v-for="child in item.childs"
-          class="flex flex-col font-montserrat-semi-bold leading-8 text-cta-dark"
-          :class="item.color"
-        >
-          <a href="/" class="whitespace-nowrap">
-            {{ child.title }}
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
+  <nav class="fixed bg-cta-dark top-20 left-0 right-0 bottom-auto pb-4 w-full">
+    <ul class="flex flex-col items-stretch justify-start h-auto mt-3 box-border max-w-[50%]">
+      <li
+        v-for="item in mainItems"
+        :data-top-id="item.blogid"
+        :key="item.blogid"
+        class="py-1 initial flex flex-col basis-full hover:bg-white font-montserrat-semi-bold leading-8 text-white"
+        :class="item.colorhover">
+        <a href="/" class="mr-6 whitespace-nowrap ml-auto">{{ item.name }}</a>
+        <div
+          :data-menu-id="item.blogid"
+          class="hidden absolute top-0 bottom-0 left-1/2 right-0 group-hover:bg-white">
+          <ul class="w-full p-8 grid grid-cols-2" id="sousmenu">
+            <li
+              v-for="child in item.items"
+              :key="child.ID"
+              class="text-cta-dark"
+              :class="item.colorhover">
+              <a :href="child.url">{{ child.title }}</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </nav>
 </template>
+<style>
+li.initial {
+  position: initial;
+}
+
+#sousmenu a:not([href^='https://www.marche.be'])::after {
+  background-image: url("../images/external.svg");
+  content: "";
+  display: inline-block;
+  vertical-align: middle;
+  width: 1.2rem;
+  height: 1.2rem;
+  background-size: 100%;
+  margin-inline-start: 0.8rem;
+  opacity: 0.8;
+}
+
+li[data-top-id='1']:hover > div,
+li[data-top-id='2']:hover > div,
+li[data-top-id='3']:hover > div,
+li[data-top-id='4']:hover > div,
+li[data-top-id='5']:hover > div,
+li[data-top-id='6']:hover > div,
+li[data-top-id='7']:hover > div,
+li[data-top-id='8']:hover > div,
+li[data-top-id='11']:hover > div,
+li[data-top-id='12']:hover > div,
+li[data-top-id='13']:hover > div,
+li[data-top-id='14']:hover > div {
+  opacity: 100;
+  background-color: white;
+  display: block;
+}
+</style>
