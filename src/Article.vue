c5d09a49d924a1a1eca27525a2ad7bd71fa1fec5
jfsenechal <jfsenechal@gmail.com>
2022-10-20 22:18:08 +0200

    clean

--- src/Article.vue
+++ src/Article.vue
@@ -1,14 +1,26 @@
 <script setup>
-import Article from "@/components/Article/Article.vue";
+import { ref } from "vue";
 import Header from "@/components/Header/Header.vue";
+import HeaderArticle from "@/components/Article/Header.vue";
+import Titre from "@/components/Article/Titre.vue";
+import Body from "@/components/Article/Body.vue";
+import PopupSearch from "@/components/Header/Search/PopupSearch.vue";
 import Footer from "@/components/Footer/Footer.vue";
+
+const searchIsOpen = ref(false);
+
+function updateSearch() {
+  searchIsOpen.value = !searchIsOpen.value;
+}
+
 </script>
 <template>
-  <Header />
-  <main>
-    <section class="container mx-auto h-screen place-content-center px-4">
-      <Article />
-    </section>
-  </main>
+  <Header @open-search="updateSearch" />
+  <PopupSearch @open-search="updateSearch" :searchIsOpen="searchIsOpen" />
+  <article class="container pt-8 xl:pt-28 mx-auto h-screen place-content-center px-4">
+    <Titre />
+    <HeaderArticle />
+    <Body />
+  </article>
   <Footer />
 </template>
