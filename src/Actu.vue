a0ee9dcf5b167ae18fd7cc0cd0b12ea3c2c600d5
jfsenechal <jfsenechal@gmail.com>
2022-10-21 20:07:17 +0200

    sm ok

--- src/Actu.vue
+++ src/Actu.vue
@@ -64,13 +64,13 @@ onMounted(() => {
             alt=""
             class="justify-self-center self-center h-32 w-full rounded-t-lg opacity-100 transition-all duration-700 group-hover:scale-105 group-hover:opacity-60 lg:h-40"
           />
-          <div class="ml-2 flex flex-col items-start justify-between h-36 xl:p-4">
+          <div class="ml-2 flex flex-col items-start justify-between h-36 py-2 xl:py-4 xl:px-4">
             <h3
               class="xl:text-md xl:font-montserrat-semi-bold text-md text-cta-dark group-hover:text-cta-light"
             >
               {{ item.post_title }}
             </h3>
-            <span class="text-greydark text-sm">{{ item.post_excerpt }}</span>
+            <span class="text-greydark text-sm font-montserrat-light">{{ item.post_excerpt }}</span>
             <span
               :class="
               'my-1 block font-montserrat-medium text-xs xl:text-base ' +
