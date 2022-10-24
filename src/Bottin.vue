ee196f6cacc5de5d121acf5e56219eb2262e62e8
jfsenechal <jfsenechal@gmail.com>
2022-10-24 20:57:27 +0200

    bottin pc ok

---
+++ src/Bottin.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from "vue";
+import Header from "@/components/Header/Header.vue";
+import Titre from "@/components/Article/Titre.vue";
+import Share from "@/components/Article/Share.vue";
+import SeeAlso from "@/components/Article/SeeAlso.vue";
+import Tags from "@/components/Article/Tags.vue";
+import Body from "@/components/Article/Body.vue";
+import PopupSearch from "@/components/Header/Search/PopupSearch.vue";
+import Footer from "@/components/Footer/Footer.vue";
+import Encare from "@/components/Bottin/Encare.vue";
+
+const searchIsOpen = ref(false);
+
+function updateSearch() {
+  searchIsOpen.value = !searchIsOpen.value;
+}
+</script>
+<template>
+  <Header @open-search="updateSearch" />
+  <PopupSearch @open-search="updateSearch" :searchIsOpen="searchIsOpen" />
+  <article
+    class="container grid grid-cols-1 xl:grid-cols-3 items-start mt-24 xl:mt-28 mx-auto place-content-center px-4">
+    <Titre />
+    <Encare />
+    <Tags />
+    <Body />
+    <Share />
+    <SeeAlso />
+  </article>
+  <Footer />
+</template>
---
+++ src/components/Bottin/Encare.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+<template>
+  <div
+    class="grid grid-cols-[60%_40%] my-2 xl:mr-4 h-60 xl:h-80 overflow-hidden xl:row-start-2 xl:row-end-5 xl:col-start-1 xl:col-end-3">
+    <picture>
+      <img
+        src="https://bottin.marche.be/bottin/fiches/693/311f9677386734af25faf967b4255179.jpg"
+        alt="img"
+        class="object-top"
+      />
+    </picture>
+    <div class="bg-greylighter flex flex-col justify-around p-4">
+
+      <address class="flex flex-col">
+        <span class="font-montserrat-bold">Adresse</span>
+        Avenue de la Toison d'Or 112 <br />
+        6900 Marche-en-Famenne
+      </address>
+      <div class="flex flex-col">
+        <span class="font-montserrat-bold">Téléphone</span>
+        <span class="">084 31 32 43</span>
+      </div>
+      <div class="flex flex-col">
+        <span class="font-montserrat-bold">Adresse e-mail</span>
+        <a href="mailto:jf@marche.be">jf@marche.be</a>
+      </div>
+      <div class="flex flex-col">
+        <span class="font-montserrat-bold">Site web</span>
+        <a href="">https://www.marche.be</a>
+      </div>
+    </div>
+  </div>
+</template>
