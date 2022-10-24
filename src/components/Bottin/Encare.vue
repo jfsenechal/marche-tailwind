bcc6c98fe0e6b776b6ff3761dc1f616d1e8ee8c3
jfsenechal <jfsenechal@gmail.com>
2022-10-24 21:13:25 +0200

    bottin sm ok

--- src/components/Bottin/Encare.vue
+++ src/components/Bottin/Encare.vue
@@ -3,8 +3,8 @@
 </script>
 <template>
   <div
-    class="grid grid-cols-[60%_40%] my-2 xl:mr-4 h-60 xl:h-80 overflow-hidden xl:row-start-2 xl:row-end-5 xl:col-start-1 xl:col-end-3">
-    <picture>
+    class="grid grid-cols-1 xl:grid-cols-[60%_40%] my-3 xl:my-2 xl:mr-4 h-60 xl:h-80 overflow-hidden xl:row-start-2 xl:row-end-5 xl:col-start-1 xl:col-end-3">
+    <picture class="hidden xl:block">
       <img
         src="https://bottin.marche.be/bottin/fiches/693/311f9677386734af25faf967b4255179.jpg"
         alt="img"
@@ -20,7 +20,7 @@
       </address>
       <div class="flex flex-col">
         <span class="font-montserrat-bold">Téléphone</span>
-        <span class="">084 31 32 43</span>
+        <a href="tel:084 31 32 43" class="">084 31 32 43</a>
       </div>
       <div class="flex flex-col">
         <span class="font-montserrat-bold">Adresse e-mail</span>
@@ -28,7 +28,7 @@
       </div>
       <div class="flex flex-col">
         <span class="font-montserrat-bold">Site web</span>
-        <a href="">https://www.marche.be</a>
+        <a href="https://www.marche.be">https://www.marche.be</a>
       </div>
     </div>
   </div>
