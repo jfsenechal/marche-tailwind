efb850a68518bd62bf3938fb5fba8e32bc20482d
jfsenechal <jfsenechal@gmail.com>
2022-12-07 15:37:24 +0100

    multiple see also

--- src/components/Article/Image.vue
+++ src/components/Article/Image.vue
@@ -3,7 +3,7 @@
   <div class="my-2 xl:mr-4 h-60 xl:h-80 overflow-hidden xl:row-start-2 xl:row-end-5 xl:col-start-1 xl:col-end-3">
     <picture>
       <img
-        src="images/Pietonnier-renovation.jpg"
+        src="https://www.marche.be/wp-content/uploads/2022/11/ErasNoel340x158_Plan-de-travail-1-1-scaled.jpg"
         alt="img"
         class="object-top"
       />
--- src/components/Article/SeeAlso.vue
+++ src/components/Article/SeeAlso.vue
@@ -3,7 +3,55 @@
   <div class="my-6 xl:row-start-4 xl:row-end-5 xl:col-start-3 xl:col-end-4">
     <h4 class="font-montserrat-bold text-cta-dark">Consultez aussi</h4>
     <div class="mt-6">
-      <ul role="list" class="mt-3">
+      <ul role="list" class="mt-3 flex flex-col gap-2">
+        <li class="group rounded-md shadow-lg">
+          <a href="#" class="flex">
+            <div
+              class="flex flex-shrink-0 items-center justify-center rounded-l-md transition-all duration-500 group-hover:opacity-75"
+            >
+              <img src="images/BEA-r_long.jpg" alt="img" class="h-28 w-28" />
+            </div>
+            <div
+              class="rounded-r-md border-t border-r border-b border-gray-200 bg-white w-full"
+            >
+              <div
+                class="flex h-full flex-col justify-between px-4 py-4 text-sm"
+              >
+                <a
+                  href="#"
+                  class="block font-semibold text-cta-dark group-hover:text-cta-light"
+                >
+                  Journée du Bien-Être Animal (25 septembre)</a
+                >
+                <p class="block font-montserrat-bold text-citoyen">Citoyen</p>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li class="group rounded-md shadow-lg">
+          <a href="#" class="flex">
+            <div
+              class="flex flex-shrink-0 items-center justify-center rounded-l-md transition-all duration-500 group-hover:opacity-75"
+            >
+              <img src="images/BEA-r_long.jpg" alt="img" class="h-28 w-28" />
+            </div>
+            <div
+              class="rounded-r-md border-t border-r border-b border-gray-200 bg-white w-full"
+            >
+              <div
+                class="flex h-full flex-col justify-between px-4 py-4 text-sm"
+              >
+                <a
+                  href="#"
+                  class="block font-semibold text-cta-dark group-hover:text-cta-light"
+                >
+                  Journée du Bien-Être Animal (25 septembre)</a
+                >
+                <p class="block font-montserrat-bold text-citoyen">Citoyen</p>
+              </div>
+            </div>
+          </a>
+        </li>
         <li class="group rounded-md shadow-lg">
           <a href="#" class="flex">
             <div
